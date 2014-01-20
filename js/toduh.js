f0f5ab9ad94d5cee6c1a6d1dd714d2147edd71ce
cheri allen <cherimarie@gmail.com>
2014-01-19 19:58:10 -0800

    Minor refactoring and comment tidying

--- js/toduh.js
+++ js/toduh.js
@@ -12,10 +12,10 @@ function dateAdded(){
   return dayMonthYear;
 }
 
-//initialize array of all things
+//initialize array of things
 var allThings = [];
 
-//function to add thing from form to list and to array, triggered by clicking #addThing button  
+//function to add thing, triggered by clicking #addThing button  
 function addThing(){
   var newDesc = document.getElementById("newThing").value;
 
@@ -26,7 +26,7 @@ function addThing(){
     var newThing = new Thing(newDesc);
     allThings.push(newThing);
 
-    //print the newThing to list of things
+    //print the newThing to bottom of list of things
     var table = document.getElementById("listThings");
     var row = table.insertRow(-1);
     row.id = "thing" + allThings.indexOf(newThing);
@@ -37,7 +37,7 @@ function addThing(){
     var cell3 = row.insertCell(2);
     cell3.innerHTML = "<i class='fa fa-minus-square'></i>" 
     cell3.onclick = function(){
-      deleteRow(newThing)
+      deleteThing(newThing)
     };
     var cell4 = row.insertCell(3);
     cell4.innerHTML = "<i class='fa fa-pencil-square-o'></i>";
@@ -47,7 +47,6 @@ function addThing(){
   }
 }
 
-//edit function
 function edit(thing){
   //create edit box at bottom of container, with save and cancel buttons
   var editBox = document.createElement("div");
@@ -59,6 +58,7 @@ function edit(thing){
   editText.value = thing.description;
   saveButton.innerHTML = "Save";
   cancelButton.innerHTML = "Cancel";
+
   saveButton.onclick = function(){
     saveEdit(thing, editText.value);
   };
@@ -95,17 +95,12 @@ function clearEditBox(){
 }
 
 //delete function
-function deleteRow(thing){
+function deleteThing(thing){
     alert("Are you sure you no longer care for this thing?");
     var index = allThings.indexOf(thing);
     document.getElementById("listThings").deleteRow(index + 1);
     allThings.remove(index);
 }
-
-//when page loads, display initial list 
-document.addEventListener( "DOMContentLoaded", function(e){
-
-});
 
 // Array Remove - By John Resig (MIT Licensed)
 Array.prototype.remove = function(from, to) {
